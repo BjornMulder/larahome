aa0550f9b4b8ba7ed688ff0fe94e571dff655f67
bjornmulderdij <bjorn.mulder@deinternetjongens.nl>
2018-08-31 19:38:24 +0200

    Adds basic models and migrations

---
+++ database/migrations/2018_08_31_172748_create_sequence_actions_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSequenceActionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sequence_actions', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sequence_actions');
+    }
+}
