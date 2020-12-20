f340f05421b59df4bd9fc310ff58a8e2a4a7e9c4
Bjorn Mulder <bjorn.mulder@deinternetjongens.nl>
2020-12-20 18:48:17 +0100

    Adds try-put statechanged controller

---
+++ app/Http/Controllers/StateChangedController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class StateChangedController extends Controller
+{
+
+    /**
+     * undocumented function
+     *
+     * @return void
+     */
+    public function __invoke(Request $request)
+    {
+        dd($request);
+    }
+
+}
