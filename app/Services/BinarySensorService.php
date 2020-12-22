b8523ba63a064993625c63904ae69d17ba3beec9
Bjorn Mulder <bjorn.mulder@deinternetjongens.nl>
2020-12-22 12:29:13 +0100

    Adds bathroom sensor flow

--- app/Services/BinarySensorService.php
+++ app/Services/BinarySensorService.php
@@ -10,10 +10,12 @@ class BinarySensorService
 {
     private $hassApiService;
     private $timerService;
-    public function __construct(HassApiService $hassApiService, TimerService $timerService)
+    private $entityStateService;
+    public function __construct(HassApiService $hassApiService, TimerService $timerService, EntityStateService $entityStateService)
     {
         $this->hassApiService = $hassApiService;
         $this->timerService = $timerService;
+        $this->entityStateService = $entityStateService;
     }
 
     public function handle($entity) {
@@ -22,5 +24,16 @@ class BinarySensorService
             $this->hassApiService->callService('light', 'turn_on', ['entity_id' => 'light.closet'] );
             $this->timerService->set("light.closet", 'light', 'turn_off', 300);
         }
+
+        // Bathroom light flow
+        if ($entity->entity_id === "binary_sensor.bathroom_sensor" && $entity->state === "on") {
+            $this->hassApiService->callService('light', 'turn_on', ['entity_id' => 'light.bathroom'] );
+            $this->timerService->set("light.bathroom", 'light', 'turn_off', 120);
+        }
+        if ($entity->entity_id === "binary_sensor.bathroom_radar" && $entity->state === "on") {
+            $this->entityStateService->checkState('light.bathroom', "on");
+            $this->timerService->set("light.bathroom", 'light', 'turn_off', 120);
+        }
+
     }
 }
---
+++ app/Services/EntityStateService.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Services;
+
+use App\Models\Entity;
+
+class EntityStateService
+{
+    public function checkState($entity_id, $expectedState) {
+        if (Entity::where('entity_id', $entity_id)->pluck('state') === $expectedState) {
+            return true;
+        }
+        return false;
+    }
+}
