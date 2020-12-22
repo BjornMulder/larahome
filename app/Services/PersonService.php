e4f2a07aadec56b9978ee3b07cc74f6b9ca9bca4
Bjorn Mulder <bjorn.mulder@deinternetjongens.nl>
2020-12-22 14:29:14 +0100

    adds google home service

--- app/Services/PersonService.php
+++ app/Services/PersonService.php
@@ -4,18 +4,21 @@ declare(strict_types=1);
 
 namespace App\Services;
 
+use App\Http\Services\GoogleHomeService;
 use App\Http\Services\HassApiService;
 
 class PersonService
 {
     private $hassApiService;
+    private $googleHomeService;
     private $timerService;
     private $entityStateService;
-    public function __construct(HassApiService $hassApiService, TimerService $timerService, EntityStateService $entityStateService)
+    public function __construct(HassApiService $hassApiService, TimerService $timerService, EntityStateService $entityStateService, GoogleHomeService $googleHomeService)
     {
         $this->hassApiService = $hassApiService;
         $this->timerService = $timerService;
         $this->entityStateService = $entityStateService;
+        $this->googleHomeService = $googleHomeService;
     }
 
     public function handle($entity) {
@@ -23,14 +26,16 @@ class PersonService
         // Sarah leaves
         if ($entity->entity_id === "person.sarah" && $entity->state === "away") {
             if ($this->entityStateService->checkState('person.bjorn_mulder', "away")) {
-                $this->hassApiService->callService('light', 'light.turn_off', ['entity_id' => 'all'] );
+                //$this->hassApiService->callService('light', 'light.turn_off', ['entity_id' => 'all'] );
+                $this->googleHomeService->run("Turn off all lights");
             }
         }
 
         //  Bjorn Leaves
         if ($entity->entity_id === "person.bjorn_mulder" && $entity->state === "away") {
             if ($this->entityStateService->checkState('person.sarah', "away")) {
-                $this->hassApiService->callService('light', 'light.turn_off', ['entity_id' => 'all'] );
+                //$this->hassApiService->callService('light', 'light.turn_off', ['entity_id' => 'all'] );
+                $this->googleHomeService->run("Turn off all lights");
             }
         }
     }
--- config/hass.php
+++ config/hass.php
@@ -4,6 +4,6 @@ return [
 
     'api_base_url' => env('HASS_API_URL'),
     'api_token' => env('HASS_API_TOKEN'),
-
+    'google_home_base_url' => env('GOOGLE_HOME_BASE_URL'),
 ];
 
