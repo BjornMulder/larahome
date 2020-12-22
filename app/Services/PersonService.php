6762f49d78f1ee1e5f44d55b96b2bc699ea22f1e
Bjorn Mulder <bjorn.mulder@deinternetjongens.nl>
2020-12-22 14:42:13 +0100

    You geused it

--- app/Services/PersonService.php
+++ app/Services/PersonService.php
@@ -22,11 +22,11 @@ class PersonService
 
     public function handle($entity) {
 
-        dump("handlingpersonservice");
-        dump($entity);
         // Sarah leaves
         if ($entity->entity_id === "person.sarah" && $entity->state === "away") {
+            dump("sarah leaving");
             if ($this->entityStateService->checkState('person.bjorn_mulder', "away")) {
+                dump("sarah leaving and bjorn not home");
                 //$this->hassApiService->callService('light', 'light.turn_off', ['entity_id' => 'all'] );
                 $this->googleHomeService->run("Turn off all lights");
             }
@@ -34,7 +34,9 @@ class PersonService
 
         //  Bjorn Leaves
         if ($entity->entity_id === "person.bjorn_mulder" && $entity->state === "away") {
+            dump("bjorn leaving");
             if ($this->entityStateService->checkState('person.sarah', "away")) {
+                dump("bjorn leaving and sarah not home");
                 //$this->hassApiService->callService('light', 'light.turn_off', ['entity_id' => 'all'] );
                 $this->googleHomeService->run("Turn off all lights");
             }
